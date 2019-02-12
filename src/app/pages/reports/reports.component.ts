be43abd19c3975e035a22db5cd2d54f95b9be18d
alisaosx <aliasosx@hotmail.com>
2019-02-12 19:22:04 +0700

    udpate report

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,3 +1,9 @@
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Transaction } from './../../interfaces/transaction';
+import { AngularFirestoreCollection } from '@angular/fire/firestore';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { AngularFirestore } from 'angularfire2/firestore';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore, private dialog: MatDialog, private snackbarRef: MatSnackBar) {
+    this.transactionsRef = db.collection<Transaction>('transactions', ref => {
+      return ref.orderBy('orderId', 'asc');
+    });
+  }
+
+  transactionsRef: AngularFirestoreCollection<Transaction>;
+  transactions: Observable<any[]>;
 
   ngOnInit() {
+    this.transactions = this.transactionsRef.snapshotChanges().pipe(map(change => {
+      return change.map(a => {
+        const transactions = a.payload.doc.data() as Transaction;
+        transactions['id'] = a.payload.doc.id;
+        return transactions;
+      });
+    }));
   }
 
 }

1b8ba0ac79a99b500b58abcba22df4904128ad71
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2019-02-14 01:27:52 +0700

    update dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -16,17 +16,18 @@ import { async } from '@angular/core/testing';
 export class DashboardComponent implements OnInit {
   constructor(private db: AngularFirestore) {
     this.transactionsRef = db.collection<Transaction>('transactions');
-    let currentDate = firestore.Timestamp.fromDate(new Date());
+    //let currentDate = firestore.Timestamp.fromDate(new Date());
     //console.log(currentDate.toDate());
     this.transactionsCurrentRef = db.collection<Transaction>('transactions', ref => {
       return ref.orderBy('transaction_date', 'asc');
     });
-
+    this.transactionsKitchenRef = db.collection<Transaction>('transactions');
     this.kitchensRef = db.collection<Kitchen>('kitchens');
   }
 
   transactionsRef: AngularFirestoreCollection<Transaction>
   transactions: Observable<any[]>;
+
   transctionsByKitchens: Observable<any[]>;
 
   transactionCount: number = 0;
@@ -35,12 +36,16 @@ export class DashboardComponent implements OnInit {
   transactionsCurrentRef: AngularFirestoreCollection<Transaction>
   transactionsCurrent: Observable<any[]>;
 
+  transactionsKitchenRef: AngularFirestoreCollection<Transaction>;
+  transactionsKitchens: Observable<any[]>;
+
   transactionCountDairy: number = 0;
   transactionAmountDiary: number = 0;
 
   kitchensRef: AngularFirestoreCollection<Kitchen>;
   kitchens: Observable<any[]>;
   reportByKitchen: any[] = [];
+  transactionCurrentKitchen: Observable<any[]>;
 
   ngOnInit() {
     this.transactions = this.transactionsRef.snapshotChanges().pipe(map(change => {
@@ -77,8 +82,16 @@ export class DashboardComponent implements OnInit {
         }
       });
     });
-
     // load Kitchen list
+    this.transactionsKitchens = this.transactionsKitchenRef.snapshotChanges().pipe(map(change => {
+      return change.map(a => {
+        const data = a.payload.doc.data() as Transaction;
+        data['id'] = a.payload.doc.id;
+        return data;
+      });
+    }));
+
+
     this.kitchens = this.kitchensRef.snapshotChanges().pipe(map(change => {
       return change.map(a => {
         const data = a.payload.doc.data() as Kitchen;
@@ -87,38 +100,29 @@ export class DashboardComponent implements OnInit {
       });
     }));
 
-    this.kitchens.subscribe(async (kitchens) => {
-      let _kitchenName = "";
+    this.kitchens.subscribe(kitchens => {
       kitchens.forEach(kitchen => {
-        this.db.collection<Transaction>('transactions', ref => {
-          _kitchenName = kitchen.kitchenName;
-          console.log(kitchen.kitchenName);
-          return ref.where('kitchen', '==', kitchen.kitchenName);
-        }).snapshotChanges().pipe(map(change => {
-          return change.map(a => {
-            //console.log(a.payload.doc.data());
-            const data = a.payload.doc.data() as Transaction;
-            data['id'] = a.payload.doc.id;
-            return data;
-          });
-        })).subscribe(async (tranxs) => {
+        this.transactionsKitchens.subscribe(tranxs => {
           let kitchenCount = 0;
           let kitchenAmount = 0;
-          let c = await tranxs.forEach(tranx => {
-            kitchenCount += tranx.quantity;
-            kitchenAmount += tranx.total_price;
+
+          tranxs.forEach(tranx => {
+            let tranx_date = new DatePipe('en-us').transform(tranx.transaction_date.toDate(), 'dd-MMM-yyyy');
+            let currentDate = new DatePipe('en-us').transform(new Date(), 'dd-MMM-yyyy');
+            if (tranx_date === currentDate) {
+              if (tranx.kitchen == kitchen.kitchenName) {
+                kitchenCount += tranx.quantity;
+                kitchenAmount += tranx.total_price;
+              }
+            }
           });
-          console.log(_kitchenName);
           this.reportByKitchen.push({
-            kitchen: _kitchenName,
+            kitchen: kitchen.kitchenName,
             kitchenCount: kitchenCount,
             kitchenAmount: kitchenAmount
           });
-          console.log(this.reportByKitchen);
-
         });
       });
     });
-    //console.log(this.reportByKitchen);
   }
 }

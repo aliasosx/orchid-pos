732ac096960b902bd2343a6321cd54a66d54445e
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2019-03-01 01:34:30 +0700

    add console

--- src/app/pages/dbconsole/dbconsole.component.ts
+++ src/app/pages/dbconsole/dbconsole.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Transaction } from 'src/app/interfaces/transaction';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dbconsole',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DbconsoleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
+
+  }
+  updateTransactionsLogfix(foodId) {
+    this.db.collection<Transaction>('transactions').snapshotChanges().pipe(map(change => {
+      return change.map(a => {
+        const data = a.payload.doc.data() as Transaction;
+        data['id'] = a.payload.doc.id;
+        return data;
+      });
+    })).subscribe(tx => {
+      tx.forEach(t => {
+        if (t.foodId == foodId) {
+          this.db.collection<Transaction>('transactions').doc(t.id).update({
+            cost: 5800,
+            total_cost: 5800 * 1,
+            profit: 13000 - 5800,
+          }).then(() => {
+            console.log('Transaction log updated');
+          });
+        }
+      })
+    });
   }
 
 }
--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -237,27 +237,7 @@ export class OrdersComponent implements OnInit {
     }
   }
 
-  updateTransactionsLogfix(foodId) {
-    this.db.collection<Transaction>('transactions').snapshotChanges().pipe(map(change => {
-      return change.map(a => {
-        const data = a.payload.doc.data() as Transaction;
-        data['id'] = a.payload.doc.id;
-        return data;
-      });
-    })).subscribe(tx => {
-      tx.forEach(t => {
-        if (t.foodId == foodId) {
-          this.db.collection<Transaction>('transactions').doc(t.id).update({
-            cost: 5800,
-            total_cost: 5800 * 1,
-            profit: 13000 - 5800,
-          }).then(() => {
-            console.log('Transaction log updated');
-          });
-        }
-      })
-    });
-  }
+
 
   async stockUpdate(order) {
     let productCurrentQuantity = 0;

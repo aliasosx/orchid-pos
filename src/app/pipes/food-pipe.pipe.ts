fe7f80816e84d315220c2ca808393b3fe1f3de63
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2019-04-14 00:19:11 +0700

    update pipe

--- src/app/pipes/food-pipe.pipe.ts
+++ src/app/pipes/food-pipe.pipe.ts
@@ -7,7 +7,8 @@ import { Food } from '../interfaces/food';
 export class FoodPipePipe implements PipeTransform {
 
   transform(value: any[], foodname: string): any {
-    if (!foodname) return value;
+    console.log(value);
+    if (!foodname) { return value; }
     return value.filter(food => food['food_name'].indexOf(foodname) != -1);
   }
 

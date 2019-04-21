f17f91d29986cadc5e89a9bd0b596edfc395a579
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2019-04-21 18:30:16 +0700

    update dashboard

---
+++ src/app/interfaces/notices.ts
@@ -0,0 +1,5 @@
+export interface Notice {
+  message: string;
+  noticeBy: string;
+  noticeDate: Date;
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -10,6 +10,9 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Router } from '@angular/router';
 import { Food } from 'src/app/interfaces/food';
 import { BackendServiceService } from 'src/app/services/common/backend-service.service';
+import { Notice } from 'src/app/interfaces/notices';
+import { Message } from 'src/app/interfaces/messages';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -19,6 +22,12 @@ import { BackendServiceService } from 'src/app/services/common/backend-service.s
 export class DashboardComponent implements OnInit {
   // tslint:disable-next-line: max-line-length
   constructor(private db: AngularFirestore, private _firebaseAuth: AngularFireAuth, private router: Router, private be: BackendServiceService) {
+    this.noticeRef = db.collection<Notice>('notices', ref => {
+      return ref.orderBy('noticeDate', 'desc');
+    });
+    this.messageRef = db.collection<Message>('messages', ref => {
+      return ref.orderBy('dateTime', 'desc');
+    });
   }
 
   private user: Observable<firebase.User>;
@@ -31,7 +40,40 @@ export class DashboardComponent implements OnInit {
   summaryReportsPayments: any;
   summaryReportsUsers: any;
   roleId = JSON.parse(localStorage.getItem('usrObj')).roleId;
+
+  noticeRef: AngularFirestoreCollection<Notice>;
+  notices: Observable<any[]>;
+
+  messageRef: AngularFirestoreCollection<Message>;
+  messages: Observable<any[]>;
+
+
+  messageForm: FormGroup;
+
   ngOnInit() {
+
+    this.messageForm = new FormGroup({
+      userName: new FormControl(JSON.parse(localStorage.getItem('usrObj')).fullname),
+      dateTime: new FormControl(new Date()),
+      message: new FormControl(),
+    });
+
+    this.notices = this.noticeRef.snapshotChanges().pipe(map(change => {
+      return change.map(a => {
+        const notices = a.payload.doc.data() as Notice;
+        notices['id'] = a.payload.doc.id;
+        return notices;
+      });
+    }));
+
+    this.messages = this.messageRef.snapshotChanges().pipe(map(change => {
+      return change.map(a => {
+        const messages = a.payload.doc.data() as Message;
+        messages['id'] = a.payload.doc.id;
+        return messages;
+      });
+    }));
+
     this.loadDiaryTransactionsAmount();
   }
   reloadReport() {
@@ -67,6 +109,18 @@ export class DashboardComponent implements OnInit {
         this.summaryReportsUsers = r;
       });
     });
+  }
+  sendMsg(event) {
+    if (event.key === 'Enter') {
+      console.log(this.messageForm.value);
+      if (this.messageForm.get('message').value) {
+        this.db.collection('messages').add(this.messageForm.value).then(() => {
+          this.messageForm.get('message').reset();
+          console.log('Message send');
+        });
+      }
+
+    }
   }
 
 }

2e212925cf0d744a8b8addf32be1558294e82e7e
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2019-05-17 12:55:18 +0700

    update deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,3 @@
 echo "Deployment starting...";
 ng build --prod --base-href /; cd dist/orchid-pos/;scp -r * root@139.59.192.33:/var/www/html
-echo "Deployment complete.";
+echo "############################# Deployment complete. #############################";

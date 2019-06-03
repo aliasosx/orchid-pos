33bba136d7a8e79335b94660253892d428165653
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2019-06-04 00:17:18 +0700

    update purchase ui

--- src/app/pages/purchase-grid/purchase-grid.component.ts
+++ src/app/pages/purchase-grid/purchase-grid.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { StockServicesService } from 'src/app/services/stock-services.service';
 import { BackendServiceService } from 'src/app/services/common/backend-service.service';
+import { MatDialog } from '@angular/material';
+import { AddProductsComponent } from 'src/app/dialogs/add-products/add-products.component';
 
 @Component({
   selector: 'app-purchase-grid',
@@ -10,7 +12,7 @@ import { BackendServiceService } from 'src/app/services/common/backend-service.s
 })
 export class PurchaseGridComponent implements OnInit {
 
-  constructor(private stockService: StockServicesService, private backendService: BackendServiceService) { }
+  constructor(private stockService: StockServicesService, private backendService: BackendServiceService, private dialog: MatDialog) { }
   searchForm: FormGroup;
   productCategories: any;
   products: any;
@@ -30,5 +32,13 @@ export class PurchaseGridComponent implements OnInit {
       rsp.subscribe(products => this.products = products);
     });
   }
-
+  openAddNewProd() {
+    const dialogRef = this.dialog.open(AddProductsComponent, {
+      width: '600px',
+    });
+    dialogRef.afterClosed().subscribe(r => {
+      this.loadProductCategories();
+      this.loadProducts();
+    });
+  }
 }

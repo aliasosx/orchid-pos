fa3f9290ae2f63fc8910818955759e78b12681ac
alisaosx <aliasosx@hotmail.com>
2019-06-04 15:50:08 +0700

    update purchase module

---
+++ src/app/interfaces/purchaseBuffer.ts
@@ -0,0 +1,10 @@
+export interface PurchaseBuffer {
+  billingNo: String;
+  productId: number;
+  product_name: String;
+  cost: number;
+  quantity: number;
+  total: number;
+  purchaseDate: Date;
+  userId: number;
+}
--- src/app/pages/purchase-grid/purchase-grid.component.ts
+++ src/app/pages/purchase-grid/purchase-grid.component.ts
@@ -1,9 +1,15 @@
+import { map } from 'rxjs/operators';
+import { AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore } from 'angularfire2/firestore';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { StockServicesService } from 'src/app/services/stock-services.service';
 import { BackendServiceService } from 'src/app/services/common/backend-service.service';
 import { MatDialog } from '@angular/material';
 import { AddProductsComponent } from 'src/app/dialogs/add-products/add-products.component';
+import { FirebaseFirestore } from 'angularfire2';
+import { PurchaseBuffer } from 'src/app/interfaces/purchaseBuffer';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-purchase-grid',
@@ -12,15 +18,31 @@ import { AddProductsComponent } from 'src/app/dialogs/add-products/add-products.
 })
 export class PurchaseGridComponent implements OnInit {
 
-  constructor(private stockService: StockServicesService, private backendService: BackendServiceService, private dialog: MatDialog) { }
+  // tslint:disable-next-line: max-line-length
+  constructor(private stockService: StockServicesService, private backendService: BackendServiceService, private dialog: MatDialog, private db: AngularFirestore) {
+    this.purchaseBufferRef = db.collection<PurchaseBuffer>('purchaseBuffers');
+  }
   searchForm: FormGroup;
   productCategories: any;
   products: any;
   selectedCate: any;
 
+  purchaseBufferRef: AngularFirestoreCollection<PurchaseBuffer>;
+  purchaseBuffers: Observable<any>;
+  grandTotal = 0;
+  items = [];
   ngOnInit() {
     this.loadProductCategories();
     this.loadProducts();
+    this.purchaseBuffers = this.purchaseBufferRef.snapshotChanges().pipe(map(change => {
+      this.grandTotal = 0;
+      return change.map(a => {
+        const purchases = a.payload.doc.data() as PurchaseBuffer;
+        purchases['id'] = a.payload.doc.id;
+        this.grandTotal += a.payload.doc.data().total;
+        return purchases;
+      });
+    }));
   }
   async loadProductCategories() {
     this.stockService.getProductCategories().then(rsp => {
@@ -40,5 +62,43 @@ export class PurchaseGridComponent implements OnInit {
       this.loadProductCategories();
       this.loadProducts();
     });
+  }
+  takePurchase(product) {
+    this.db.collection('purchaseBuffers').ref.get().then(doc => {
+    });
+    const purchase_data = {
+      billingNo: 10000,
+      productId: product.id,
+      product_name: product.product_name,
+      cost: product.cost,
+      quantity: 1,
+      total: product.cost,
+      purchaseDate: new Date(),
+      userId: JSON.parse(localStorage.getItem('usrObj')).id,
+    };
+    this.db.collection('purchaseBuffers').add(purchase_data).then(rsp => {
+      // console.log(rsp);
+    });
+  }
+  removeItem(id) {
+    this.db.collection('purchaseBuffers').doc(id).delete();
+  }
+  updateItemQuantity(id, quantity, cost) {
+    const data = {
+      quantity: quantity,
+      total: quantity * cost
+    };
+    this.db.collection('purchaseBuffers').doc(id).update(data);
+  }
+  updateItemCost(id, quantity, cost) {
+    const data = {
+      cost: cost,
+      quantity: quantity,
+      total: quantity * cost
+    };
+    this.db.collection('purchaseBuffers').doc(id).update(data);
+  }
+  caculateGrandTotal(total) {
+    console.log(total);
   }
 }

cb9a8a2bbcc50a86046063885e68b82958bf4205
aliasosx <sayyalinh@outlook.com>
2019-08-16 01:06:10 +0700

    update report add Goteddy

--- src/app/pages/kitchen-report-admin/kitchen-report-admin.component.ts
+++ src/app/pages/kitchen-report-admin/kitchen-report-admin.component.ts
@@ -22,6 +22,11 @@ export class KitchenReportAdminComponent implements OnInit {
 
   grandTotal = 0;
   countItem = 0;
+
+  deriverGrandTotal = 0;
+  deriverCountItem = 0;
+
+
   startDate: Date;
   endDate: Date;
   foodList: any[] = [];
@@ -30,6 +35,8 @@ export class KitchenReportAdminComponent implements OnInit {
   paymentSummaries: any;
   totalPaymentSumm = 0;
   dateFrom: FormGroup;
+  payment_by_kitchen_derivery: any;
+  payment_by_kitchen_deriveries: any[] = [];
 
   ngOnInit() {
     this.dateFrom = new FormGroup({
@@ -42,11 +49,17 @@ export class KitchenReportAdminComponent implements OnInit {
   async loadReport() {
     this.startDate = this.dateFrom.get('initDate').value;
     this.endDate = this.dateFrom.get('endDate').value;
+
+    this.deriverCountItem = 0;
+    this.deriverGrandTotal = 0;
+
     // console.log('OK');
     this.paymentCount = 0;
     this.paymentSummary = 0;
+
     this.be.reportsKitchenAdmin(this.dateFrom.get('initDate').value, this.dateFrom.get('endDate').value, 'Food').then(rsp => {
       this.foodList = [];
+      this.payment_by_kitchen_deriveries = [];
       let _total = 0;
       let _count = 0;
 
@@ -55,18 +68,31 @@ export class KitchenReportAdminComponent implements OnInit {
       let _paymentTotal = 0;
       let _paymentCount = 0;
 
+
+      let _total_deriveries = 0;
+      let _count_deriveries = 0;
+
       rsp.subscribe(r => {
         this.foodsTransactions = r['reports'];
         this.payments = r['report_payment_by_kitchen'];
+        this.payment_by_kitchen_derivery = r['payment_by_kitchen_derivery'];
         this.paymentList = [];
         this.foodList.push(r['reports']);
+        this.payment_by_kitchen_deriveries.push(r['payment_by_kitchen_derivery']);
         this.foodList.forEach(f => {
           f.forEach(element => {
             _total += parseInt(element.total_cost, 10);
             _count += parseInt(element.quantity, 10);
           });
         });
-
+        _total_deriveries = 0;
+        _count_deriveries = 0;
+        this.payment_by_kitchen_deriveries.forEach(el => {
+          el.forEach(e => {
+            _total_deriveries += parseInt(e.total_cost, 10);
+            _count_deriveries += parseInt(e.quantity, 10);
+          });
+        });
         this.paymentList.push(r['report_payment_by_kitchen']);
         this.paymentList.forEach(p => {
           p.forEach(element => {
@@ -83,6 +109,10 @@ export class KitchenReportAdminComponent implements OnInit {
 
         this.grandTotal = _total;
         this.countItem = _count;
+
+        this.deriverGrandTotal = _total_deriveries;
+        this.deriverCountItem = _count_deriveries;
+
 
         this.paymentCount = _paymentCount;
         this.paymentSummary = _paymentTotal;
